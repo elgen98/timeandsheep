7238e5991a561415458ba7f9e4af5195b892adf5
elgen98 <elliot.blomqvist@yahoo.com>
2021-10-04 16:12:38 +0200

    minified CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
+var cleanCSS = require("gulp-clean-css");
 
 gulp.task("compile", function () {
   return gulp.src("SCSS/main.scss").pipe(sass()).pipe(gulp.dest("CSS"));
@@ -7,6 +8,10 @@ gulp.task("compile", function () {
 
 gulp.task("watch-scss", function () {
   gulp.watch("SCSS/main.scss", gulp.series("compile"));
+});
+
+gulp.task("minify-css", () => {
+  return gulp.src("CSS/main.css").pipe(cleanCSS()).pipe(gulp.dest("dist"));
 });
 
 gulp.task("default", gulp.series("compile", "watch-scss"), function () {});
